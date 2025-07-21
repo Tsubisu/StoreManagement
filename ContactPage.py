a5602bd46700d095650e2fb5bf1d160745f1969e
Udhir <messiudhir10@gmail.com>
2025-07-21 16:29:58 +0545

    cahnged the visuals of contact page

--- ContactPage.py
+++ ContactPage.py
@@ -1,4 +1,5 @@
 from tkinter import ttk as ttk
+from tkinter import *
 from tkinter import Text, messagebox
 import sqlite3
 
@@ -17,43 +18,60 @@ def contact_page(frame):
     conn.close()
     
 def contact_page(frame):
+    
     contact_style=ttk.Style()
-    contact_style.configure("contact.TFrame", background="red")
+    contact_style.configure("contact.TFrame", background="white")
     contact_page_frame= ttk.Frame(frame,width=1200,height=(675-120),style="contact.TFrame")
     contact_page_frame.pack_propagate(False)
     
      
-    name_label = ttk.Label(contact_page_frame, text="Name:", background="red", foreground="white", font=("Arial", 12))
-    name_label.place(x=30, y=50)
-    name_entry = ttk.Entry(contact_page_frame, width=50)
-    name_entry.place(x=120, y=50)
+    name_entry = ttk.Label(contact_page_frame, background="white", foreground="black", font=("Arial", 12))
+    name_entry =ttk.Entry(contact_page_frame, width=55)
+    
+    name_entry.place(x=210, y=100,height=40)
 
     
-    email_label = ttk.Label(contact_page_frame, text="Email:", background="red", foreground="white", font=("Arial", 12))
-    email_label.place(x=30, y=90)
-    email_entry = ttk.Entry(contact_page_frame, width=50)
-    email_entry.place(x=120, y=90)
+    email_entry = ttk.Label(contact_page_frame, background="white", foreground="black", font=("Arial", 12))
+    email_entry = ttk.Entry(contact_page_frame, width=60)
+
+    email_entry.place(x=570, y=100,height=40)
 
     
-    contact_label = ttk.Label(contact_page_frame, text="Contact:", background="red", foreground="white", font=("Arial", 12))
-    contact_label.place(x=30, y=130)
-    contact_entry = ttk.Entry(contact_page_frame, width=50)
-    contact_entry.place(x=120, y=130)
-
-    comment_label = ttk.Label(contact_page_frame, text="Comment:", background="red", foreground="white", font=("Arial", 12))
-    comment_label.place(x=30, y=180)
-    comment_text = Text(contact_page_frame, font=("Arial", 12), wrap="word")
-    comment_text.place(x=120, y=180, width=305, height=100)
+    contact_entry = ttk.Label(contact_page_frame, background="white", foreground="black", font=("Arial", 12))
+    contact_entry = ttk.Entry(contact_page_frame, width=120)
     
-   
+    contact_entry.place(x=210, y=170,height=40)
+    
+    comment_text = Text(contact_page_frame, font=("Arial", 10), wrap="word")
+    comment_text.place(x=210, y=230, width=730, height=120)
+    
+    comment_text.config(foreground='black', relief="solid", bd=1)
+    
+    def default_text():
+        if name_entry.get()!="Name":
+            name_entry.delete(0, "end")
+            name_entry.insert(0, "Name")
+        if email_entry.get!="Email":
+            email_entry.delete(0, "end")
+            email_entry.insert(0, "Email")
+        if contact_entry.get()!="Contact":
+            contact_entry.delete(0, "end")
+            contact_entry.insert(0, "Contact")
+        if comment_text!="Comment":
+            comment_text.delete("1.0", "end")
+            comment_text.insert("1.0", "Comment")
+            
+    default_text()
+
     def save_data():
         name = name_entry.get()
         email = email_entry.get()
         contact = contact_entry.get()
         comment = comment_text.get("1.0", "end-1c")
 
-        if not name or not email or not contact or not comment:
+        if name=="Name" or email=="Email" or contact=="Contact" or comment=="Comment":
             messagebox.showerror("Error", "All fields are required.")
+            default_text()
             return
 
         conn = sqlite3.connect("store.db")
@@ -68,9 +86,9 @@ def contact_page(frame):
         email_entry.delete(0, "end")
         contact_entry.delete(0, "end")
         comment_text.delete("1.0", "end")
-        
+        default_text()
     
-    send_btn = ttk.Button(contact_page_frame, text="Send",takefocus=False,command=save_data)
-    send_btn.place(x=30, y=320)
+    send_btn = Button(contact_page_frame,background="black",foreground="white",width="20" ,text="Send", takefocus=False,command=save_data)
+    send_btn.place(x=220, y=400)
     
     return contact_page_frame
