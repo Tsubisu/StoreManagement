11a7c559fefb87d9e8eaab0e669a471184a41aaf
Udhir <messiudhir10@gmail.com>
2025-07-25 18:08:18 +0545

    change in order

--- CheckComments.py
+++ CheckComments.py
@@ -1,9 +1,13 @@
 from tkinter import ttk
 import sqlite3
+
+
 def check_comments(frame):
     check_style=ttk.Style()
     check_style.configure("check.TFrame", background="white")
     check_page_frame= ttk.Frame(frame,width=1200,height=(675-120),style="check.TFrame")
     return check_page_frame
+
+
     
     
